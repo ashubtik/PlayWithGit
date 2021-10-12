fe9ebe3bffe1840c0b1b55ce02d69e89cb9bd5e6
Artyom <ashubtik@gmail.com>
2021-10-12 13:24:13 +0300

    Delete files

--- practice/src/main/java/trying/AMain.java
+++ practice/src/main/java/trying/AMain.java
@@ -7,13 +7,19 @@ import java.util.stream.Collectors;
 
 public class AMain {
     public static void main(String[] args) {
-        StringFunction exclaim = (s) -> s + "!";
-        StringFunction ask = (s) -> s + "?";
-        printFormatted("Hello", exclaim);
-        printFormatted("Hello", ask);
-    }
-    public static void printFormatted(String str, StringFunction format) {
-        String result = format.run(str);
-        System.out.println(result);
+        List<Integer> intsList = new ArrayList<>();
+        intsList.add(43);
+        intsList.add(211);
+        intsList.add(12);
+        intsList.add(9);
+        intsList.add(3426);
+        intsList.add(998);
+        intsList.add(90);
+        intsList.add(32);
+        intsList.add(649);
+        intsList.add(9);
+        intsList.add(43);
+
+        System.out.println(intsList.stream().distinct().sorted().collect(Collectors.toList()));
     }
 }
